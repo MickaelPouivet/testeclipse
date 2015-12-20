ee02d0df008d8b2e6c3e65052e72b80fa0537f1f
Mickael Pouivet <mickael.pouivet@gmail.com>
2015-12-20 15:39:05 -0500

    Fichier source initial

---
+++ src/Searching.java
@@ -0,0 +1,12 @@
+public class Searching {
+	public Integer exampleProp = 1;
+	public String exampleProp2 = "Hey";
+
+	public void example() {
+		this.example2();
+	}
+
+	public void example2() {
+		exampleProp2 = "prop value changed here";
+	}
+}
